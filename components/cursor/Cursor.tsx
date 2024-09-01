3e1b54e558ea00cbf593caee06415c9009fc9e61
jk <jkwak1635@gmail.com>
2024-09-01 20:07:23 +0900

    feat: add conditional message display

--- components/cursor/Cursor.tsx
+++ components/cursor/Cursor.tsx
@@ -1,18 +1,29 @@
 import CursorSVG from "@/public/assets/CursorSVG";
 
-type Props = {
+type CursorProps = {
   color: string;
   x: number;
   y: number;
+  message: string;
 };
 
-const Cursor = ({ color, x, y }: Props) => {
+const Cursor = ({ color, x, y, message }: CursorProps) => {
   return (
     <div
       className='pointer-events-none absolute left-0 top-0'
       style={{ transform: `translateX(${x}px) translateY(${y}px)` }}
     >
       <CursorSVG color={color} />
+      {message && (
+        <div
+          className='absolute left-2 top-5 rounded-3xl px-4 py-2'
+          style={{ backgroundColor: color }}
+        >
+          <p className='whitespace-nowrap text-sm leading-relaxed text-white'>
+            {message}
+          </p>
+        </div>
+      )}
     </div>
   );
 };
