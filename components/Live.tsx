688de74c2b3a43e818a9a7fc0bd836de877a9da7
jk <jkwak1635@gmail.com>
2024-09-01 19:35:16 +0900

    feat: add cursor chat

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,30 +1,35 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import LiveCursors from "@/components/cursor/LiveCursors";
 import { useOthers } from "@liveblocks/react/suspense";
 import { useMyPresence } from "@liveblocks/react";
+import { CursorMode } from "@/types/type";
+import CursorChat from "@/components/cursor/CursorChat";
 
 const Live = () => {
   const others = useOthers();
   const [{ cursor }, updateMyPresence] = useMyPresence();
 
-  const handlePointerMove = useCallback((event: React.PointerEvent) => {
-    event.preventDefault();
-    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
-    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+  const [cursorState, setCursorState] = useState({
+    mode: CursorMode.Hidden,
+  });
+
+  const handlePointerMove = useCallback((e: React.PointerEvent) => {
+    e.preventDefault();
+    const x = e.clientX - e.currentTarget.getBoundingClientRect().x;
+    const y = e.clientY - e.currentTarget.getBoundingClientRect().y;
 
     updateMyPresence({ cursor: { x, y } });
   }, []);
 
-  const handlePointerLeave = useCallback((event: React.PointerEvent) => {
-    event.preventDefault();
-
+  const handlePointerLeave = useCallback((e: React.PointerEvent) => {
+    setCursorState({ mode: CursorMode.Hidden });
     updateMyPresence({ cursor: null, message: null });
   }, []);
 
-  const handlePointerDown = useCallback((event: React.PointerEvent) => {
-    event.preventDefault();
-    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
-    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+  const handlePointerDown = useCallback((e: React.PointerEvent) => {
+    e.preventDefault();
+    const x = e.clientX - e.currentTarget.getBoundingClientRect().x;
+    const y = e.clientY - e.currentTarget.getBoundingClientRect().y;
 
     updateMyPresence({ cursor: { x, y } });
   }, []);
@@ -37,6 +42,14 @@ const Live = () => {
       className='flex h-[100vh] w-full items-center justify-center text-center'
     >
       <h1 className='text-2xl text-white'>Figma Clone</h1>
+      {cursor && (
+        <CursorChat
+          cursor={cursor}
+          cursorState={cursorState}
+          setCursorState={setCursorState}
+          updateMyPresence={updateMyPresence}
+        />
+      )}
       <LiveCursors others={others} />
     </div>
   );
