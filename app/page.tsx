bbc5b367b9b8845a0db2568e97e542040043ba54
jk <jkwak1635@gmail.com>
2024-09-02 01:47:50 +0900

    feat: add sidebars to both left and right sides

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,17 @@
 
 import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
+import LeftSidebar from "@/components/LeftSidebar";
+import RightSidebar from "@/components/RightSidebar";
 
 export default function Page() {
   return (
     <main className='h-screen overflow-hidden'>
       <Navbar />
       <section className='flex h-full flex-row'>
+        <LeftSidebar />
         <Live />
+        <RightSidebar />
       </section>
     </main>
   );
---
+++ components/RightSidebar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const RightSidebar = () => {
+  return (
+    <section className='sticky left-0 flex h-full min-w-[227px] select-none flex-col overflow-y-auto border-t border-primary-grey-200 bg-primary-black pb-20 text-primary-grey-300 max-sm:hidden'>
+      <h3 className='px-5 pt-4 text-xs uppercase'>Design</h3>
+    </section>
+  );
+};
+export default RightSidebar;
