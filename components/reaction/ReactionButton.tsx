0330a3ddd5b83a392ec1eb4adbee7403f704795c
jk <jkwak1635@gmail.com>
2024-09-01 20:22:40 +0900

    feat: copy example reaction code from docs

---
+++ components/reaction/ReactionButton.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+type ReactionButtonProps = {
+  reaction: string;
+  onSelect: (reaction: string) => void;
+};
+
+const ReactionButton = ({ reaction, onSelect }: ReactionButtonProps) => {
+  return (
+    <button
+      className='transform select-none p-2 text-xl transition-transform hover:scale-150 focus:scale-150 focus:outline-none'
+      onPointerDown={() => onSelect(reaction)}
+    >
+      {reaction}
+    </button>
+  );
+};
+
+export default ReactionButton;
