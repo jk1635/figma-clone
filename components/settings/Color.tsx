0f2ceb42410aeec31cf750f22f9e3111c7b37623
jk <jkwak1635@gmail.com>
2024-10-05 23:46:55 +0900

    feat: add Color component

--- components/settings/Color.tsx
+++ components/settings/Color.tsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { Label } from "@/components/ui/label";
 
-const Color = () => {
-  return <div>Color</div>;
+type ColorProps = {
+  inputRef: any;
+  attribute: string;
+  placeholder: string;
+  attributeType: string;
+  handleInputChange: (property: string, value: string) => void;
+};
+
+const Color = ({
+  inputRef,
+  attribute,
+  placeholder,
+  attributeType,
+  handleInputChange,
+}: ColorProps) => {
+  return (
+    <div className='borer-b flex flex-col gap-3 border-primary-grey-200 p-5'>
+      <h3 className='text-[10px] uppercase'>{placeholder}</h3>
+      <div
+        className='flex items-center gap-2 border border-primary-grey-200'
+        onClick={() => inputRef.current.click()}
+      >
+        <input
+          type='color'
+          value={attribute}
+          ref={inputRef}
+          onChange={(e) => handleInputChange(attributeType, e.target.value)}
+        />
+        <Label className='flex-1'>attribute</Label>
+        <Label className='flex h-6 w-8 items-center justify-center bg-primary-grey-100 text-[10px] leading-3'>
+          90%
+        </Label>
+      </div>
+    </div>
+  );
 };
 
 export default Color;
